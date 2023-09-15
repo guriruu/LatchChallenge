9a18d194f7ce54c8b5e00401702d3a46f5f212c7
guri <santi.rudi@gmail.com>
2023-09-15 17:44:53 -0300

    latch challenge

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./data/latch.key -out ./data/latch.crt -subj "/C=AR/ST=BA/L=BA/O=YourOrg/OU=BA/CN=latchchallenge.local"
 docker build -t latchchallenge:v1 . && docker-compose up -d
